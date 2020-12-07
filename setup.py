8c11ddba75b1bf7b4796dda41cf9ef785bc94b82
Eragold <26120554+Eragold@users.noreply.github.com>
2020-12-07 17:21:00 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
       name='genetic',
       version='1.0',
       packages=[
-      'numpy>=1.0',
-      'deap=>1.2',
-      'scikit-learn=>0.17']
+      'numpy',
+      'deap',
+      'scikit-learn']
 )
